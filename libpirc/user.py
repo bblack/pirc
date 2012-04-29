ca91fa7a7bd82629c2427bae12db7c4e88331ae0
Brian Black <black@gatech.edu>
2012-04-28 21:41:01 -0400

    Proper parsing of user string

--- libpirc/user.py
+++ libpirc/user.py
@@ -1,7 +1,19 @@
+import re
+
 class User:
   def __init__(self, ustr):
-    #HACK
-    self.nick = ustr.split('!', 1)[0]
+    pattern = '(?P<nick_or_server>[^!@]*)(!(?P<user>[^@]*))?(@(?P<host>.*))?'
+    m = re.match(pattern, ustr)
+    
+    if m.group('nick_or_server') != None:
+      if '.' in m.group('nick_or_server'):
+        self.nick = None
+        self.server = m.group('nick_or_server')
+      else:
+        self.nick = m.group('nick_or_server')
+        self.server = None
+    self.user = m.group('user')
+    self.host = m.group('host')
 
   @staticmethod
   def parse(ustr):
